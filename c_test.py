140ddf8e056d6d5486cb9e53d5a693824d3fc767
Jimmy <yelo@yaoi.se>
2011-01-25 20:56:40 +0100

    modified:   c_test.png 	modified:   c_test.py

--- c_test.py
+++ c_test.py
@@ -6,15 +6,15 @@ def main():
     tc = escc.escapeColors()
 
     tc.textcolor(fg="YELLOW")
-    print("Text-color!")
+    print("Text color!")
     tc.reset()
 
     tc.textcolor(bg="BLUE")
-    print("Background-color!")
+    print("Background color!")
     tc.reset()
 
     tc.textcolor(bgmode="DIM", fg="BLUE", bg="YELLOW")
-    print("We even have mixed-mode!")
+    print("We even have a fancy mixed mode!")
     tc.reset()
 
 if __name__ == "__main__":
